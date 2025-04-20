0d8c9b56e430c146387ad82f1b5ab7622b2dc946
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-04-20 19:23:22 -0400

    daily leetcode problem (dynamic programming)

---
+++ solutions/300-LongestIncreasingSubsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+
+        return max(LIS)
+
+'''
+1. Iterate through the array backward (i), have a nested for loop for a certain segment (j)
+2. As we iterate through that segment, check if j > i, if so that segment is strictly increasing
+'''
