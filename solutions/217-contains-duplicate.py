e482ca3f5c361d57dd93c895585733ea7a25ce03
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-06-22 23:04:27 -0400

    update

--- solutions/217-contains-duplicate.py
+++ solutions/217-contains-duplicate.py
@@ -1,4 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        temp = set(nums)
-        return True if len(temp) != len(nums) else False
+        return len(set(nums)) != len(nums)
