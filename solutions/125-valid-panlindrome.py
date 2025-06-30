e4779ab494d83f5e987910a76e8a0c24077df175
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-06-30 12:28:13 -0400

    update

--- solutions/125-valid-panlindrome.py
+++ solutions/125-valid-panlindrome.py
@@ -1,20 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s)-1
-        
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        left,right = 0,len(s)-1
 
-        while l < r:
-            while l < r and not self.alphaNum(s[l]):
-                l += 1
-            while l < r and not self.alphaNum(s[r]):
-                r -= 1
-            if s[r].lower() != s[l].lower():
+        while left < right:
+            if s[left] != s[right]:
                 return False
-            l += 1
-            r -= 1
-        return True
+            left+=1
+            right-=1
         
-    def alphaNum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or 
-                ord('a') <= ord(c) <= ord('z') or 
-                ord('0') <= ord(c) <= ord('9'))
+        return True
